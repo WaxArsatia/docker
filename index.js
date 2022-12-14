b26416cdc6577152167efae97f50951214aba04b
WaxArsatia <waxarsatia@gmail.com>
2022-12-14 20:58:18 +0700

    Testing dockerize git project 9

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import express from 'express';
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Its Works New')
+  res.send('Its Works New 2')
 })
 
 app.listen(80, () => console.log('Express Ready'))
